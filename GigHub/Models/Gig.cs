734e944f81afcdcebff1b0c9fc4ffc8f03ca7268
Mohammed Sharique <Mohammed Sharique@SHARIQUES-PC>
2017-09-30 22:44:53 -0500

    Create the gig model.

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+    }
+
+   
+}
